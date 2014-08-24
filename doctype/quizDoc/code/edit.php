7732d69f48ed1e9de6753ad6c10d1ef853d81b55
mat1234561 <mbonfig52@gmail.com>
2014-08-23 21:04:35 -0500

    quizdoc

---
+++ doctype/quizDoc/code/edit.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="edit.css">
+        <title></title>
+    </head>
+    <body>
+		<script src="edit.js"></script>
+		<div class="ribbon">
+		<input type="button" class="buttonN" onclick="rename()" value="<?php echo $_GET['file']; ?>">
+		<input type="button" class="button" value="NEW">
+		<input type="button" class="button" value="SAVE">
+		<input type="button" class="button" value="SHARE">
+		<input type="button" class="button" value="VIEW">
+		<input type="button" class="button" value="RESPONCES">
+		<input type="button" class="button" value="TEST">
+		<input type="button" class="button" value="STUDY">
+		</div>
+        
+    </body>
+</html>
